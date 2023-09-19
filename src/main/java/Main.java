f768996f94215a1bebee436c7a74914a1418d143
SmuuSka <samuaikio@gmail.com>
2023-09-19 19:33:03 +0300

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Laskin laskin = new Laskin();
+        double etaisyys = laskin.laskeEtaisyys(2, 5, 7, 2);
+        System.out.println("Etäisyys: " + etaisyys);
+    }
+}
