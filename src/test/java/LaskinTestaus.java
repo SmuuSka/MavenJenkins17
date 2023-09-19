b4557eef93b7144e47217d1f751c4143087bac6a
SmuuSka <samuaikio@gmail.com>
2023-09-19 21:38:18 +0300

    init commit

--- src/test/java/LaskinTestaus.java
+++ src/test/java/LaskinTestaus.java
@@ -2,11 +2,11 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class Laskin1Test {
+public class LaskinTestaus {
     private static double DELTA = 0.001;
-    Laskin1 laskin = new Laskin1();
+    private Laskin1 laskin = new Laskin1();
     @Test
-    public void laskeEtaisyys() {
+    public void testLaskeEtaisyys() {
         assertEquals("Testataan pituutta", 5.0, laskin.laskeEtaisyys(1,1,4,5),DELTA);
     }
 }
